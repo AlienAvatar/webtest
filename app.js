73b09916b00f5f3d66f34ad4af4f65a27282c361
Avatar <767808421@163.com>
2019-02-26 19:53:45 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var createError = require('http-errors');
+var express = require('express');
+const app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const goRouter = require('./routes/go');
+const mainRouter = require('./routes/main')
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//路由
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/go',goRouter);
+app.use('/main',mainRouter);
+
+
+app.listen(8080);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
